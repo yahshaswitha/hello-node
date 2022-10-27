c42fa6e1f5dbf652c90c6f17bdb7683e5be7d12a
Harsh Verma <vharshverma18@gmail.com>
2022-10-27 16:33:10 +0530

    routes

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
-const print=()=>{
-    console.log("Hello Node.js!");
-};
+const http = require("http");
+const fs = require("fs");
 
-print()
+let registrationContent = "";
+let projectContent = "";
+
+fs.readFile("registration.html", (err, registration) => {
+  if (err) {
+    throw err;
+  }
+  registrationContent = registration;
+});
+
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+
+http
+  .createServer((request, response) => {
+    let url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
+      case "/registration":
+        response.write(registrationContent);
+        response.end();
+        break;
+    }
+  })
+  .listen(5000);
