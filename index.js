9a67dde0dfd043d85ac92c55eaed4e8908e1f81c
Harsh Verma <vharshverma18@gmail.com>
2022-10-27 17:02:10 +0530

    routes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require("http");
 const fs = require("fs");
+const args = require("minimist")(process.argv.slice(2));
 
 let registrationContent = "";
 let projectContent = "";
@@ -45,4 +46,4 @@ http
             break;
     }
   })
-  .listen(5000);
+  .listen(args['port']);
